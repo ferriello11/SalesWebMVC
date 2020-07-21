c099ae74cc3ee8f69191567c1d758fe6e18e8f22
FabianoTotvs <fabiano.faleiro@totvs.com.br>
2020-07-20 23:42:22 -0300

    SEEDING SERVICE

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -12,7 +12,7 @@ namespace SalesWebMvc.Models
         public string Name { get; set; }
         public string Email { get; set; }
         public DateTime BirthDate { get; set; }
-        public double BaseSalary { get; set; }        
+        public double BaseSalary { get; set; }
         public Department Department { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
@@ -20,6 +20,8 @@ namespace SalesWebMvc.Models
         {
 
         }
+
+
 
         public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
         {
@@ -30,6 +32,7 @@ namespace SalesWebMvc.Models
             BaseSalary = baseSalary;
             Department = department;
         }
+
 
 
         public void AddSales(SalesRecord sr)
